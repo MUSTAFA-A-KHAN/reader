8f91e3d00306a08c751279a2bcc415de31cb8423
MUSTAFA-A-KHAN <mustafakhan62608@gmail.com>
2024-05-22 07:52:32 +0530

    feat: load pdf with too much question mark

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        creame: '#FFFDD0',
+      },
+    },
+   
+  },
+  plugins: [],
+}
+
